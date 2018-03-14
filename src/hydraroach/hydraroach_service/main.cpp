3b53df34a9e68447a0c1b22df73b51d846244fc3
Hugo Wainwright <hugowainwright@fb.com>
2018-03-14 21:21:37 +0000

    removed greeter service

--- src/hydraroach/hydraroach_service/main.cpp
+++ src/hydraroach/hydraroach_service/main.cpp
@@ -1,37 +1,18 @@
-#include "hydraroach/proto/greeter.grpc.pb.h"
 #include "hydraroachservice.hpp"
 #include <grpc++/grpc++.h>
 #include <iostream>
 #include <string>
 
-class GreeterServiceImpl final : public greeter::Greeter::Service {
-  grpc::Status SayHello(grpc::ServerContext *context,
-                        const greeter::HelloRequest *request,
-                        greeter::HelloReply *reply) override {
-    std::string hello("Hello, ");
-    reply->set_message(hello + request->name());
-    return grpc::Status::OK;
-  }
-};
-
 int main() {
   std::string server_address("0.0.0.0:50051");
-  GreeterServiceImpl service;
   hydraroach::HydraRoachService hydraroachService;
 
   grpc::ServerBuilder builder;
-  // Listen on the given address without any authentication mechanism.
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  // Register "service" as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
-  builder.RegisterService(&service);
   builder.RegisterService(&hydraroachService);
-  // Finally assemble the server.
   std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
   std::cout << "Server listening on " << server_address << std::endl;
 
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
   server->Wait();
   return 0;
 }
--- src/hydraroach/simple_client/simple_client_main.cpp
+++ src/hydraroach/simple_client/simple_client_main.cpp
@@ -1,41 +1,7 @@
-#include "hydraroach/proto/greeter.grpc.pb.h"
 #include "hydraroach/replay_publish/replaypublisher.hpp"
 #include <grpc++/grpc++.h>
 #include <iostream>
 #include <string>
-
-class GreeterClient {
-public:
-  GreeterClient(const std::shared_ptr<grpc::Channel> channel)
-      : stub_(greeter::Greeter::NewStub(channel)){};
-  // Assambles the client's payload, sends it and presents the
-  // response back from the server.
-  std::string SayHello(const std::string &user) {
-    // Data we are sending to the server.
-    greeter::HelloRequest request;
-    request.set_name(user);
-
-    // Container for the data we expect from the server.
-    greeter::HelloReply reply;
-
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
-    grpc::ClientContext context;
-
-    // The actual RPC.
-    grpc::Status status = stub_->SayHello(&context, request, &reply);
-
-    // Act upon its status.
-    if (status.ok()) {
-      return reply.message();
-    } else {
-      return "RPC failed";
-    }
-  }
-
-private:
-  std::unique_ptr<greeter::Greeter::Stub> stub_;
-};
 
 int main(int argc, char *argv[]) {
 
@@ -43,14 +9,7 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
-  if (std::strcmp(argv[1], "greet") == 0) {
-    GreeterClient greeter(grpc::CreateChannel(
-        "localhost:50051", grpc::InsecureChannelCredentials()));
-
-    std::string user = argc > 2 ? std::string(argv[2]) : std::string("frugs");
-    std::string reply = greeter.SayHello(user);
-    std::cout << "Greeter received: " << reply << std::endl;
-  } else if (std::strcmp(argv[1], "announcereplay") == 0) {
+  if (std::strcmp(argv[1], "announcereplay") == 0) {
     hydraroach::ReplayPublisher replayPublisher;
     replayPublisher.PublishReplay(std::string("Hello, World"));
   }
